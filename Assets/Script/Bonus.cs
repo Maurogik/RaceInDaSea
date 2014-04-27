35ccd820d9ab825f3ceddebbc61fd34662fbaf98
Maurogik <gwenn.aubert@gmail.com>
2014-04-27 14:02:01 +0200

    base bonus class

---
+++ Assets/Script/Bonus.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bonus : MonoBehaviour {
+
+  private bool isActivated;
+
+	// Use this for initialization
+	virtual void Start () {
+    isActivated = false;
+	}
+	
+	// Update is called once per frame
+	virtual void Update () {
+	  
+	}
+
+  virtual void activate()
+  {
+
+  }
+}
