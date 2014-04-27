d61ad94b854946a22c0a253b584a4b52a84f0450
Maurogik <gwenn.aubert@gmail.com>
2014-04-27 11:10:11 +0200

    MainMenu et multijoueur

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
 
@@ -10,21 +11,47 @@ public class GameController : MonoBehaviour {
   public Terrain terrain;
 
 
-  private GameObject player;
+  private List<GameObject> players;
 
 
 
 	// Use this for initialization
 	void Start () {
-    player = (Instantiate(playerPrefab) as Transform).gameObject;
-    player.GetComponent<Control>().terrain = terrain;
-    startingBlock.StartGame(player);
+    players = new List<GameObject>();
+
+    GameConfig conf = GameConfig.GetInstance();
+
+    for (int i = 0; i < conf.players.Count; ++i)
+    {
+      GameObject player = (Instantiate(playerPrefab) as Transform).gameObject;
+      player.GetComponent<Control>().terrain = terrain;
+      player.GetComponent<Control>().playerIndex = conf.players[i];
+      startingBlock.StartGame(player);
+      players.Add(player);
+
+      Camera playerCam = player.transform.GetComponentInChildren<Camera>();
+      playerCam.rect = new Rect(0.0f, 0.0f + i * 0.5f, 1.0f, 1.0f / conf.players.Count);
+
+    }
+
+
 	}
 
-  public void EndGame()
+  public void EndGame(int player)
   {
     Debug.Log("End Game");
-    startingBlock.StartGame(player);
+
+    GameConfig.GetInstance().playersPosition.Add("Player "+(player + 1));
+
+    for (int i = 0; i < GameConfig.GetInstance().players.Count; ++i)
+    {
+      if(!GameConfig.GetInstance().playersPosition.Contains("Player "+(i + 1)))
+      {
+        return;
+      }
+    }
+    GameConfig.GetInstance().displayEndGameMenu = true;
+    Application.LoadLevel("MainMenu");
   }
 	
 	// Update is called once per frame
---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+  public GUISkin menuSkin;
+  Rect startRect;
+  Rect quitRect;
+  Rect titleRect;
+  Rect infoRect;
+  MenuState state;
+
+  enum MenuState
+  {
+    StartMenu,
+    PlayerConnectMenu,
+    EndGameMenu
+  }
+
+	// Use this for initialization
+	void Start ()
+  {
+    state = MenuState.StartMenu;
+    startRect = new Rect(Screen.width / 2 - 200, Screen.height / 2 - 300, 400, 100);
+    quitRect = new Rect(Screen.width / 2 - 200, Screen.height / 2 -150 , 400, 100);
+    titleRect = new Rect(Screen.width / 2 - 200, 50, 400, 100);
+    infoRect = new Rect(100, Screen.height / 2 , Screen.width - 200, 200);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+  {
+    if (state == MenuState.PlayerConnectMenu)
+    {
+      for (int i = 0; i < GameConfig.GetInstance().maxPlayers; ++i)
+      {
+        //Debug.Log("here");
+        float val = Input.GetAxis("H" + (i + 1));
+        bool here = i == 0 ? Input.GetKey(KeyCode.Joystick1Button0) : Input.GetKey(KeyCode.Joystick2Button0);
+        here |= Input.GetButton("Dash" + (i + 1));
+        if (here)
+        {
+          Debug.Log("connect " + i);
+          if (!GameConfig.GetInstance().players.Contains(i))
+          {
+            GameConfig.GetInstance().players.Add(i);
+          }
+        }
+        
+      }
+    }
+	
+	}
+
+  void OnGUI()
+  {
+    GUI.skin = menuSkin;
+
+    switch (state)
+    {
+      case MenuState.StartMenu :
+
+        GUI.Label(titleRect, "Main Menu");
+
+        if (GUI.Button(startRect, "Start"))
+        {
+          state = MenuState.PlayerConnectMenu;
+        }
+
+        if (GUI.Button(quitRect, "Quit"))
+        {
+          Debug.Log("Quitting game");
+          Application.Quit();
+        }
+
+        break;
+      case MenuState.PlayerConnectMenu :
+
+        GUI.Label(titleRect, "Player Connection");
+
+        GUI.Label(infoRect, "Player 1 : Press 'space', 'left shift' or joysitck button 'A' \n"
+                          + "Player 2 : Press 'enter', 'right shift' or joysitck button 'A'");
+
+        bool start = GUI.Button(startRect, "Start");
+        if (start && GameConfig.GetInstance().players.Count > 0)
+        {
+          Application.LoadLevel("RaceScene");
+        }
+
+        if (GUI.Button(quitRect, "Back"))
+        {
+          state = MenuState.StartMenu;
+        }
+
+        for (int i = 0; i < GameConfig.GetInstance().players.Count; ++i)
+        {
+          Rect boxRect = new Rect(Screen.width / 2 - 200 + 200 * i, Screen.height / 2 + 300, 180, 100);
+          GUI.Box(boxRect, "Player " + (GameConfig.GetInstance().players[i] + 1));
+        }
+
+        break;
+    }
+
+
+  }
+}
